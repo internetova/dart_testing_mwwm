8d84253fee07207e9472c3fbaea94b82fdbf8883
internetova <internetova@yandex.ru>
2021-08-16 17:32:26 +0300

    mockito -> mocktail

--- test/data/interactor/question_interactor_test.dart
+++ test/data/interactor/question_interactor_test.dart
@@ -1,42 +1,32 @@
 import 'package:dart_testing/data/data/mock.dart';
 import 'package:dart_testing/data/interactor/question_interactor.dart';
 import 'package:dart_testing/data/repository/question_repository.dart';
-import 'package:dart_testing/domain/question.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
 class QuestionRepositoryMock extends Mock implements QuestionRepository {}
 
 void main() {
-  late final List<Question> dataMock;
   late final QuestionRepositoryMock repositoryMock;
   late final QuestionInteractor interactor;
 
-  late Question question;
-
   setUp(() {
-    dataMock = questionsMock;
     repositoryMock = QuestionRepositoryMock();
     interactor = QuestionInteractor(repositoryMock);
   });
 
   test('Получить данные', () {
-    when(repositoryMock.getQuestions()).thenAnswer((_) => dataMock);
-
-    expect(interactor.getQuestions(), dataMock);
+    when(repositoryMock.getQuestions).thenAnswer((_) => questionsMock);
+    expect(interactor.getQuestions(), questionsMock);
   });
 
   test('Получить данные по id', () {
-    when(repositoryMock.getQuestion(2)).thenAnswer(
-      (_) {
-        question = dataMock.firstWhere((element) => element.id == 2);
-        return question;
-      },
-    );
+    when(() => repositoryMock.getQuestion(1))
+        .thenAnswer((_) => questionsMock.first);
 
     expect(
-      interactor.getQuestion(2),
-      question.id == 2,
+      interactor.getQuestion(1),
+      equals(questionsMock.first),
     );
   });
 }
